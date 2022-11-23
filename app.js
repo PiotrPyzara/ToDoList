f19857b54b290d6a0adb50308f9f4555bfd9ab6a
Piotr Pyzara <xxxppdkxxx@gmail.com>
2022-11-23 21:17:34 +0100

    Add database connection

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const MONGODB_URI =
+  'mongodb+srv://admin:vnUDPAUzwu1bQXcN@cluster0.x6il2.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 
-app.listen(3000);
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(process.env.PORT | 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
