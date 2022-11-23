e82bb0e199ae655b9515a607a6269ae3132e0848
Piotr Pyzara <xxxppdkxxx@gmail.com>
2022-11-23 21:44:16 +0100

    Add endpoint / in routers/index.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const indexRouters = require('./routers/index');
+
 const MONGODB_URI =
   'mongodb+srv://admin:vnUDPAUzwu1bQXcN@cluster0.x6il2.mongodb.net/todolist?retryWrites=true&w=majority';
 
 const app = express();
+
+app.use(indexRouters);
 
 mongoose
   .connect(MONGODB_URI)
