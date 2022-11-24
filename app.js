67764dfbee2e8b4d139e3933318c762baecb7204
Piotr Pyzara <xxxppdkxxx@gmail.com>
2022-11-24 11:42:00 +0100

    Add pageTitle variable  in all views

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', 'views');
 app.use(indexRouters);
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: 'Nie znaleziono strony' });
 });
 
 mongoose
--- controllers/task.js
+++ controllers/task.js
@@ -3,7 +3,10 @@ const Task = require('../models/task');
 exports.getIndex = (req, res, next) => {
   Task.find()
     .then((tasks) => {
-      res.render('index', { tasks: tasks });
+      res.render('index', {
+        pageTitle: 'Lista rzeczy do zrobienia',
+        tasks: tasks,
+      });
     })
     .catch((err) => {
       console.log(err);
