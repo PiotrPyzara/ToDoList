c9e8ae5311ffa1f8ee17a160eba3c9ddfeffaf23
Piotr Pyzara <xxxppdkxxx@gmail.com>
2022-11-24 11:58:57 +0100

    Add error handler in app.js

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.set('views', 'views');
 
 app.use(indexRouters);
 
+// error handler
+app.use('/500', errorController.get500);
 app.use(errorController.get404);
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).render('500', { pageTitle: 'Error!' });
+});
 
 mongoose
   .connect(MONGODB_URI)
