a71eca83d8089698ba5cc31142fe93029b8c1be4
Piotr Pyzara <xxxppdkxxx@gmail.com>
2022-11-24 16:13:39 +0100

    Modifty routers/index.js file by adding endpoints
    Add endpoints:
    
    // GET /
    
    // POST /task/create
    
    // GET /task/edit/:taskID
    
    // POST /task/edit
    
    // POST /task/delete

--- routers/index.js
+++ routers/index.js
@@ -6,4 +6,16 @@ const taskController = require('../controllers/task');
 // GET /
 router.get('/', taskController.getIndex);
 
+// POST /task/create
+router.get('/task/create');
+
+// GET /task/edit/:taskID
+router.get('/task/edit/:taskID');
+
+// POST /task/edit
+router.post('/task/edit');
+
+// POST /task/delete
+router.post('/task/delete');
+
 module.exports = router;
